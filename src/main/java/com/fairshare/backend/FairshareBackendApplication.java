6969f529306d1b34049755579dd4b5e7cd42bf3d
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-25 14:49:08 +0700

    Initial Project

---
+++ src/main/java/com/fairshare/backend/FairshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fairshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairshareBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FairshareBackendApplication.class, args);
+	}
+
+}
